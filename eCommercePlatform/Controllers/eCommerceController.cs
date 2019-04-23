2decf6f703514fb1629477a109942f43af57cddc
danielarmao <42427229+danielarmao@users.noreply.github.com>
2019-04-23 12:44:24 -0700

    Testing Controllers

---
+++ eCommercePlatform/Controllers/eCommerceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eCommercePlatform.Controllers
+{
+    public class ECommerceController : Controller
+    {
+        
+        public string Detail()
+        {
+            return "Hello World";
+        }
+    }
+}
